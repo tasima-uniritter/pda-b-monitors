ec747485e3793df716b927eb38d43d6354023879
Renan Drabach <rmdrabach@gmail.com>
2018-06-26 23:16:15 -0300

    Adds model for Alert. Adds new fields in Rule to control timeout.

---
+++ src/main/java/br/edu/uniritter/monitors/model/Alert.java
@@ -0,0 +1,58 @@
+package br.edu.uniritter.monitors.model;
+
+public class Alert {
+    private String origin;
+    private String metric;
+    private int value;
+    private String rule;
+    private String timestamp;
+    private int threshold;
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getMetric() {
+        return metric;
+    }
+
+    public void setMetric(String metric) {
+        this.metric = metric;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getRule() {
+        return rule;
+    }
+
+    public void setRule(String rule) {
+        this.rule = rule;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
+    }
+}

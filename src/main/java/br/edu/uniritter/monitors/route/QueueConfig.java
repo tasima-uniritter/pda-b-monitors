e1aa0c2b4a03026ff9e4dc145aecfb8b7affc3ee
Renan Drabach <rmdrabach@gmail.com>
2018-06-28 22:26:00 -0300

    Fix properties file location and use.

--- src/main/java/br/edu/uniritter/monitors/route/QueueConfig.java
+++ src/main/java/br/edu/uniritter/monitors/route/QueueConfig.java
@@ -5,31 +5,27 @@ import org.apache.camel.component.amqp.AMQPConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class QueueConfig {
-
+    private static final String AMQP_SERVICE_PROTOCOL = "amqps";
     private static final String AMQP_SERVICE_HOST = "b-79e24b8a-cd93-48a3-b234-9d75e05e3def-1.mq.us-east-1.amazonaws.com";
     private static final String AMQP_SERVICE_PORT = "5671";
     private static final String AMQP_SERVICE_USERNAME = "tda-monitors-b";
 
     @Bean
     AMQPConnectionDetails securedAmqpConnection() {
-
         Dotenv dotenv = Dotenv
                             .configure()
                             .ignoreIfMissing()
                             .directory("./")
                             .load();
 
-        String senhaMQ = dotenv.get("AMQP_SERVICE_PASSWORD");
-
-        if (senhaMQ.length() == 0) {
+        String amqpPwd = dotenv.get("AMQP_SERVICE_PASSWORD");
+        if (amqpPwd.length() == 0) {
             throw new IllegalArgumentException("Missing AMQP_SERVICE_PASSWORD environment variable");
         }
 
-        String uri = "amqps://" + AMQP_SERVICE_HOST + ":" + AMQP_SERVICE_PORT;
-
-        return new AMQPConnectionDetails(uri, AMQP_SERVICE_USERNAME, senhaMQ);
+        String uri = AMQP_SERVICE_PROTOCOL + "://" + AMQP_SERVICE_HOST + ":" + AMQP_SERVICE_PORT;
+        return new AMQPConnectionDetails(uri, AMQP_SERVICE_USERNAME, amqpPwd);
     }
 }
